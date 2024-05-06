ee41a4ed38d51373ccbc595d5d935e6e6cac5276
JdcStig <duffycreagh2003@gmail.com>
2024-05-06 12:59:21 +0100

    Created a classes page and added the footer and header

---
+++ resources/views/Classes.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course notes example code</title>
+    <link href="newcss.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="header">
+        <img src="https://themewagon.github.io/gymlife/img/logo.png" width="175px" height="50px" id="Logo" alt="Logo">
+        <div id="Ancher_div">
+            <a href="index.html"><h2 class="Ancher"> HOME </h2></a>
+            <a href="About_Us.html"><h2 class="Ancher"> ABOUT US </h2></a>
+            <a href="Classes.html"><h2 class="Ancher"> CLASSES </h2></a>
+            <h2 class="Ancher"> SERVICES </h2>
+            <h2 class="Ancher"> OUR TEAM </h2>
+        </div>
+    </div>
+
+    
+    
+    
+    
+    
+    <footer>
+        <div class="row_icons">
+            <div class="footer_column">
+                <img src="https://png.pngtree.com/png-clipart/20230805/original/pngtree-icon-of-an-orange-colored-flat-and-rounded-map-marker-in-vector-format-vector-picture-image_9721238.png" width="30%" height="40%">
+                <h2 class="h2_Nohover"></h2>
+            </div>
+            <div class="footer_column">
+                <img src="https://cdn-icons-png.freepik.com/512/8568/8568051.png" width="30%" height="40%">
+                <h2 class="h2_Nohover"></h2>
+            </div>
+            <div class="footer_column">
+                <img src="https://www.freepnglogos.com/uploads/email-png/email-icon-philippe-the-original-31.png" width="30%" height="40%">
+                <h2 class="h2_Nohover"></h2>
+            </div>
+        </div>
+        <section></section>
+        <div class="footer_row">
+            <div class="footer_column">
+                <img src="https://themewagon.github.io/gymlife/img/logo.png" width="175px" height="50px" id="Logo" alt="Logo">
+                <p>Contact us on 067-4566-6663 or Email us at ExampleEmail@gmail.com</p>
+            </div>
+            <div class="footer_column">
+                <h2>USEFUL LINKS</h2>
+                <p>About</p>
+                <p>Blog</p>
+                <p>Classes</p>
+                <p>Contact</p>
+            </div>
+            <div class="footer_column">
+                <h2>Tips & Guides</h2>
+                <p>Physical fitness may help prevent depression, anxiety 3 min read 20 Comment</p>
+                <p>Fitness: The best exercise to lose belly fat and tone up... 3 min read 20 Comment</p>
+            </div>
+        </div>
+    </footer>
+    
+    
+    
+    
+</body>
+</html>
