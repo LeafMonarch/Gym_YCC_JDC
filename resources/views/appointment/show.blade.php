4cca367f08fcbcc72cd2aa550218ca34e5a1fe9f
Yee Chean Chang <145558346+LeafMonarch@users.noreply.github.com>
2024-05-08 22:39:21 +0100

    Completed "Keep Reading"  on index.blade

---
+++ resources/views/appointment/show.blade.php
@@ -0,0 +1,32 @@
+{{-- @extends('layouts.app')
+
+@section('content')
+
+@endsection --}}
+
+<x-app-layout>
+    <div class="w-4/5 m-auto text-center">
+        <div class="py-15 border-b border-gray-200">
+            <h1 class="text-6xl ">
+                {{ $appointment->exercise_type }}
+            </h1>
+        </div>
+    </div>
+
+
+    <div class="w-4/5 m-auto pt-20">
+        <span class="text-gray-500">
+            By <span class="font-bold italic text-gray-800">{{ $appointment->user->name }}</span>, Created on {{ date('jS M Y', strtotime($appointment->updated_at)) }}
+        </span>
+    
+        <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+            {{ $appointment->comment }}
+        </p>
+    </div>
+
+
+
+
+
+</div>
+</x-app-layout>
